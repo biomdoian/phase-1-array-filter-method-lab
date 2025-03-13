d661c48fb37fe4388542065fa0f6cbf51bd29a0c
Ian <iankruze60@gmail.com>
2025-03-13 23:11:04 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driverNames, searchString) {
+    return driverNames.filter(name => 
+      name.toLowerCase().includes(searchString.toLowerCase())
+    );
+  }
+  
+  function fuzzyMatch(driverNames, searchString) {
+    return driverNames.filter(name => 
+      name.toLowerCase().startsWith(searchString.toLowerCase())
+    );
+  }
+  
+  function matchName(driverObjects, searchString) {
+    return driverObjects.filter(driver => 
+      driver.name === searchString
+    );
+  }
